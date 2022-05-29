d00b49fb600ead77e9075bc64cf4ee02a2bfe27d
sagar-aryal <sagar_yal92@outlook.com>
2022-05-29 17:35:43 +0300

    getting a chat message event to client side functionality added

--- index.js
+++ index.js
@@ -16,9 +16,13 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  // console.log("a user connected : " + socket.id);
+  socket.on("chat message", (msg) => {
+    //console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("user disconnected: " + socket.id);
   });
 });
 
