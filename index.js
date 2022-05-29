30c4b5602942407bb7a824a614647951dd5b5ece
sagar-aryal <sagar_yal92@outlook.com>
2022-05-29 16:59:21 +0300

    created index.html file and refactor our route handler to use sendFile instead

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import http from "http";
 
+// alternative for __dirname in Node.js when using ES6 modules
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const server = http.createServer(app);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(3000, () => {
