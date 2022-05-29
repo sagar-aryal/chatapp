d868b1d8958dd08bc3bb3bcea057f76da0093c49
sagar-aryal <sagar_yal92@outlook.com>
2022-05-29 17:24:17 +0300

    disconnect event functionality added when user leaves the chat

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
