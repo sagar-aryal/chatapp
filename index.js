9b1842462c10ccc085bc4c9558482f3720cb3c08
sagar-aryal <sagar_yal92@outlook.com>
2022-05-29 17:21:58 +0300

    integrating socket.io with a server that  mounts on Node.JS HTTP Server socket.io

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 // alternative for __dirname in Node.js when using ES6 modules
 import { dirname } from "path";
@@ -8,9 +9,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(3000, () => {
